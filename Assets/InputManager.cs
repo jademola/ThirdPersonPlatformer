5c213b8a7803febc39fdd19d3cd8830fd30ce313
Jimi Ademola <jimi.ademola1@gmail.com>
2025-02-24 18:06:39 -0800

    Added the ability to jump and coloured blocks

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -7,29 +7,38 @@ public class InputManager : MonoBehaviour
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
 
+    [SerializeField] private GameObject player;
+
+    private void Start() 
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        Rigidbody playerRB = player?.GetComponent<Rigidbody>();
+
+        if(Input.GetKeyDown(KeyCode.Space) && playerRB.transform.position.y < 1.1f)
         {
             OnSpacePressed?.Invoke();
         }
         
         // Add left and right positioning for the ball
         Vector2 input = Vector2.zero;
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             input += Vector2.left;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             input += Vector2.right;
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             input += Vector2.up;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             input += Vector2.down;
         }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float force = 1f;
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 2f;
     private Rigidbody playerRB;
 
     private void Start()
@@ -15,11 +15,19 @@ public class PlayerController : MonoBehaviour
         //Listen for horizontal movement
         inputManager.OnMove.AddListener(MovePlayer);
 
+        //Listen for space bar to allow player to jump
+        inputManager.OnSpacePressed.AddListener(JumpPlayer);
+
 
     }
 
     private void MovePlayer(Vector2 direction) {
         Vector3 moveDirection = new(direction.x, 0f, direction.y);
         playerRB.AddForce(speed * moveDirection);
+    }
+
+    private void JumpPlayer() {
+        Vector3 jumpVector = new Vector3(0, 10f, 0);
+        playerRB.AddForce(jumpVector, ForceMode.Impulse);
     }
 }
