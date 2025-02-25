20bcf7473a137b0579feabb60513b2c45b7f3527
Jimi Ademola <jimi.ademola1@gmail.com>
2025-02-25 15:38:45 -0800

    Added spinning coin objects

---
+++ Assets/CoinController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    public float speed; // Base speed of rotation
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        //transform.Rotate(Vector3.right, 2 * speed * Time.deltaTime);
+    }
+}
