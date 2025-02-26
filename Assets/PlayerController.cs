584dda208a0cfa236ca22b3c48bbeb03b2e735ad
Jimi Ademola <jimi.ademola1@gmail.com>
2025-02-25 19:54:52 -0800

    Added drag to smooth movement on ground

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
-    [SerializeField] private float force = 1f;
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float drag1;
     private Rigidbody playerRB;
+    bool grounded;
+    public LayerMask isGround;
+    public float playerHeight;
 
     private void Start()
     {
@@ -19,6 +22,19 @@ public class PlayerController : MonoBehaviour
         inputManager.OnSpacePressed.AddListener(JumpPlayer);
 
 
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f +0.2f, isGround);
+
+        if (grounded)
+        {
+            playerRB.linearDamping = drag1;
+        } else
+        {
+            playerRB.linearDamping = 0;
+        }
     }
 
     private void MovePlayer(Vector2 direction) {
