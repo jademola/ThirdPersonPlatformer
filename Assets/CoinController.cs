eefc5698d524f6a2e9f7fff68c2227fc00840fa4
Jimi Ademola <jimi.ademola1@gmail.com>
2025-02-28 10:03:32 -0800

    Add functionality for coins to destroy on impact

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -5,6 +5,7 @@ public class CoinController : MonoBehaviour
     public float speed; // Base speed of rotation
 
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +18,14 @@ public class CoinController : MonoBehaviour
 
         transform.Rotate(Vector3.up, speed * Time.deltaTime);
         //transform.Rotate(Vector3.right, 2 * speed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter(Collider triggeredObject)
+    {
+        Debug.Log("Collision!!");
+        if(triggeredObject.CompareTag("Player")) {
+            Destroy(this.gameObject);
+        }
     }
 }
